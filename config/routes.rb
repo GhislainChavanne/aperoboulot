d26022aed2a4416904e17de4f92e371fadc31f45
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-05-05 16:35:41 +0200

    formular

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'equipe',  to: 'pages#equipe'
   get 'prestations',  to: 'pages#prestation'
   match '/contacts',     to: 'contacts#new',             via: 'get'
-  resources "contacts", only: [:new, :create]
+  post 'contacts', to: 'contacts#create'
 end
