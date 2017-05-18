c5f9a19dfba1dd0989b0e653112bb1fd2b869b1f
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-05-18 10:21:56 +0200

    better front

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,21 +3,3 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 3000);
-    function checkReady() {
-        if (document.getElementsByTagName('body')[0] !== undefined) {
-            window.clearInterval(intervalID);
-            callback.call(this);
-        }
-    }
-}
-
-function show(id, value) {
-    document.getElementById(id).style.display = value ? 'block' : 'none';
-}
-
-onReady(function () {
-    show('page', true);
-    show('loading', false);
-});
