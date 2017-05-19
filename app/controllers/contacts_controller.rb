73d5cc30c94bb603fca70a3dd09cf2d161dde39e
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-05-19 10:34:10 +0200

    flash message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,9 +8,9 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      flash.now[:notice] = 'Merci pour votre message, Apéro Boulot vous repondra dans les plus brefs délais !'
     else
-      flash.now[:error] = 'Cannot send message.'
+      flash.now[:error] = "Le message n'a pas pu être envoyé"
       render :new
     end
   end
