52144951ff766bf63f6f1dac7412f25fdb20e793
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-07-11 10:38:16 +0200

    controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,13 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all.order('created_at DESC') # creates an anonymous scope
+    @articles = @articles.category(params[:category]) if params[:category].present?
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @content = Article.find(params[:id]).content
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :equipe, :prestation, :contact ]
 
   def home
+    @articles = Article.order('created_at DESC').first(3)
   end
 
   def equipe
