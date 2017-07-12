728eec0208a04d293da7d12f7d0f12c34e82e187
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-07-12 11:53:37 +0200

    better mail

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -15,7 +15,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
+      :subject => "Demande" %("#{type}"),
       :to => "gh.chavanne@gmail.com",
       :from => %("#{nom}" <#{email}>)
     }
