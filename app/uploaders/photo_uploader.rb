f2cc2f73563b45019b5e99c7e1c125b849795901
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-10-18 10:43:01 +0200

    image magic

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,7 +3,4 @@ class PhotoUploader < CarrierWave::Uploader::Base
   process eager: true  # Force version generation at upload time.
   process convert: 'jpg'
 
-  def public_id
-    return model.short_name
-  end
 end
