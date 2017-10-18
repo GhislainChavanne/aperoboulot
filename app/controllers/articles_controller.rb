b3b7b3fb39e80bb8f66e23ae36e196fbc4bf60c4
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-10-18 15:57:30 +0200

    better front

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def index
     @articles = Article.all.order('created_at DESC') # creates an anonymous scope
