83c838b7d54038c9e3896f1a10af85066ad6c19b
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-10-18 10:15:07 +0200

    better uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,5 +1,7 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  process eager: true  # Force version generation at upload time.
+  process convert: 'jpg'
 
   def public_id
     return model.short_name
